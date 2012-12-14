d55b843d7a35ebeab571f9dfa6b295a510f7ebcf
TravisBallard <ansi@home>
2012-12-13 21:23:37 -0500

    made moving box easier to see

--- inc/js/script.js
+++ inc/js/script.js
@@ -3,9 +3,11 @@ jQuery(document).ready( function($){
     var win_height = $(window).height();
     var box_width = $('#bar').width();
 
+    console.log( 'win height: ' + win_height );
+
     $(window).scroll( function(){
         var pos = ( win_height / $(window).scrollTop() ) * box_width / 100;
-        console.log( pos );
+        console.log( $(window).scrollTop() );
         $('#bar span').animate({left: pos + '%' }, 100 );
     } );
 
